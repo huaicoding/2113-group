01019da1b7e875bf0ed96e78424908a7dcf810d3
贾维斯 <apple@Jarvis.local>
2023-04-24 20:48:57 +0800

    first commit

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+//# include <string>
+//# include <cmath>
+//#include <fstream>
+//#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+    return 0;
+}
