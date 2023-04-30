2f5dcc7d084f1d63e69f58aa52ccc01c4f928186
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2023-05-01 01:22:46 +0800

    Update AllocationSystem.h

--- AllocationSystem.h
+++ AllocationSystem.h
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+
+class Node{
+  public:
+  int code;
+  Node* left;
+  Node* right;
+};
+
+class BST_Tree{
+private:
+    Node* root;
+	  Node* insertNode(AVL_Node*& tree, int code);
+	  Node* searchNode(AVL_Node* tree, int code);
+	  Node* delNode(AVL_Node*& tree, AVL_Node* code);
+    Node* maximum(AVL_Node* tree);
+		Node* minimum(AVL_Node* tree);
+  public:
+		void insertNode(int code);
+		void delNode(AVL_Node* node);
+		AVL_Node* searchNode(int code);
+};
+
+class AllocationSystem: public AVL_Tree {
+	public:
+		void Initialisation(int code);
+		void Allocate(int random);
+		void printRules();
+		void insurance(string rankst, string ranknd, double bet, double& balance);
+		string decrypter(int code);
+};
