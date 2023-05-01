a3ec98507ccaf40c6edcc08c69eaaf8551c3c013
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2023-05-01 16:21:53 +0800

    Create AllocationSystem.cpp

---
+++ AllocationSystem.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+treeNode* Insert(treeNode* currentNode, int key) {
+    if (currentNode == NULL) {
+        // Case 1: If the tree rooted at currentNode is empty
+        // To be implemented
+        currentNode = (treeNode*)malloc(sizeof(treeNode));
+        currentNode->key = key;
+        currentNode->left = NULL;
+        currentNode->right = NULL;
+    }
+    if (key > (currentNode->key)) {
+        // Case 2: If the tree rooted at currentNode is not empty and if the given key is greater than currentNode -> key
+        // To be implemented
+        currentNode->right = Insert(currentNode->right, key);
+    }
+    else if (key < (currentNode->key)) {
+        // Case 3: If the tree rooted at currentNode is not empty and if the given key is smaller than currentNode -> key
+        // To be implemented
+        currentNode->left = Insert(currentNode->left, key);
+    }
+    return currentNode;
+}
+
+void Print(treeNode* currentNode) {
+    if (currentNode == NULL) {
+        return;
+    }
+    Print(currentNode->left);
+    printf("%d ", currentNode->key);
+    Print(currentNode->right);
+}
+
+treeNode* FindMin(treeNode* currentNode) {
+    // To be implemented
+    if (currentNode->left == NULL)
+        return currentNode;
+    else
+        return FindMin(currentNode->left);
+}
+
+treeNode* FindMax(treeNode* currentNode) {
+    // To be implemented
+    if (currentNode->right == NULL)
+        return currentNode;
+    else
+        return FindMax(currentNode->right);
+}
+
+treeNode* Find(treeNode* currentNode, int key) {
+    // To be implemented
+    if (currentNode == NULL)
+        return NULL;
+    if (currentNode->key < key)
+        return Find(currentNode->right, key);
+    else if (currentNode->key > key)
+        return Find(currentNode->left, key);
+    else
+        return currentNode;
+}
