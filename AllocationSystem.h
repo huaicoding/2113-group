97dcd9db0c26d8ef51c8953b8c936a2ba35fef58
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2023-05-01 20:24:42 +0800

    Update AllocationSystem.h

--- AllocationSystem.h
+++ AllocationSystem.h
@@ -1,33 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class Node{
+class Node {
 public:
-	int code;
-  	Node* left;
-  	Node* right;
-  	Node(int code, Node* left, Node* right) : code(code), left(l), right(r) {}
+    int code;
+    Node* left;
+    Node* right;
+    Node(int code, Node* left, Node* right) : code(code), left(left), right(right) {}
 };
 
-class BST_Tree{
+class BST_Tree {
 private:
-	Node* root;
-	Node* insertNode(AVL_Node*& tree, int code);
-	Node* searchNode(AVL_Node* tree, int code);
-	Node* delNode(AVL_Node*& tree, AVL_Node* code);
-	Node* maximum(AVL_Node* tree);
-	Node* minimum(AVL_Node* tree);
+    Node* root;
+    Node* insertNode(Node*& currentNode, int code);
+    Node* searchNode(Node* currentNode, int code);
+    Node* delNode(Node*& currentNode, Node* code);
+    Node* maximum(Node* currentNode);
+    Node* minimum(Node* currentNode);
 public:
-	void insertNode(int code);
-	void delNode(AVL_Node* node);
-	AVL_Node* searchNode(int code);
+    BST_Tree();
+    void insertNode(int code);
+    void delNode(Node* node);
+    Node* searchNode(int code);
 };
 
-class AllocationSystem: public BST_Tree {
+class AllocationSystem : public BST_Tree {
 public:
-	void Initialisation(int code);
-	void Allocate(int random);
-	void printRules();
-	void insurance(string rankst, string ranknd, double bet, double& balance);
-	string decrypter(int code);
+    void Initialisation(int code);
+    void Allocate(int random);
+    void printRules();
+    void insurance(string rankst, string ranknd, double bet, double& balance);
+    string decrypter(int code);
 };
