8aacaa098464dd2e6810c2d7c4d9e4fdfb1b6306
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2023-05-01 20:26:24 +0800

    Update AllocationSystem.cpp

--- AllocationSystem.cpp
+++ AllocationSystem.cpp
@@ -3,51 +3,61 @@
 #include "AllocationSystem.h"
 using namespace std;
 
-Node* BST_Tree::insertNode(Node* &currentNode, int key) {
+BST_Tree::BST_Tree() : root(NULL){}
+
+Node* BST_Tree::insertNode(Node*& currentNode, int code) {
     if (currentNode == NULL) {
         currentNode = new Node(code, NULL, NULL);
-        currentNode->key = key;
+        currentNode->code = code;
         currentNode->left = NULL;
         currentNode->right = NULL;
     }
-    if (key > (currentNode->key)) {
-        currentNode->right = Insert(currentNode->right, key);
+    if (code > (currentNode->code)) {
+        currentNode->right = insertNode(currentNode->right, code);
     }
-    else if (key < (currentNode->key)) {
-        currentNode->left = Insert(currentNode->left, key);
+    else if (code < (currentNode->code)) {
+        currentNode->left = insertNode(currentNode->left, code);
     }
     return currentNode;
 }
 
-Node* BST_Tree::searchNode(Node* currentNode, int key) {
+void BST_Tree::insertNode(int code) {
+    insertNode(root, code);
+}
+
+Node* BST_Tree::searchNode(Node* currentNode, int code) {
     if (currentNode == NULL)
         return NULL;
-    if (currentNode->key < key)
-        return Find(currentNode->right, key);
-    else if (currentNode->key > key)
-        return Find(currentNode->left, key);
+    if (currentNode->code < code)
+        return searchNode(currentNode->right, code);
+    else if (currentNode->code > code)
+        return searchNode(currentNode->left, code);
     else
         return currentNode;
 }
 
-Node* BST_Tree::delNode(Node* &currentNode, Node* node){
+Node* BST_Tree::searchNode(int code) {
+    return searchNode(root, code);
+}
+
+Node* BST_Tree::delNode(Node*& currentNode, Node* node) {
     if (currentNode == NULL)
         return NULL;
-    else if (node->code > currentNode->code){
+    else if (node->code > currentNode->code) {
         currentNode->right = delNode(currentNode->right, node);
         return currentNode;
     }
-    else if (node->code < currentNode->code){
+    else if (node->code < currentNode->code) {
         currentNode->left = delNode(currentNode->left, node);
         return currentNode;
     }
-    else{
-        if (currentNode->left && currentNode->right){
-            Node* now = new Node(minimum(currentNode->right));
-            currentNode->code = new->code;
-            currentNode->right = delNode(currentNode->right, now); 
+    else {
+        if (currentNode->left && currentNode->right) {
+            Node* now = minimum(currentNode->right);
+            currentNode->code = now->code;
+            currentNode->right = delNode(currentNode->right, now);
         }
-        else{
+        else {
             Node* temp = currentNode;
             if (currentNode->left == NULL)
                 currentNode = currentNode->right;
@@ -56,19 +66,23 @@ Node* BST_Tree::delNode(Node* &currentNode, Node* node){
             delete temp;
         }
         return currentNode;
+    }
+}
+
+void BST_Tree::delNode(Node* node) {
+    delNode(root, node);
 }
 
 Node* BST_Tree::minimum(Node* currentNode) {
     if (currentNode->left == NULL)
         return currentNode;
     else
-        return FindMin(currentNode->left);
+        return minimum(currentNode->left);
 }
 
-Node* BST_Tree::maximum(Node* currentNode) {
+Node* BST_Tree::maximum(Node * currentNode) {
     if (currentNode->right == NULL)
         return currentNode;
     else
-        return FindMax(currentNode->right);
+        return maximum(currentNode->right);
 }
-
