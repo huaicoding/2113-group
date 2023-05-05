210b23d40613b5628bafdba9216da76df973bc7d
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2023-05-05 23:02:05 +0800

    Update banker.cpp

--- banker.cpp
+++ banker.cpp
@@ -16,26 +16,26 @@ vector<string> Banker :: get_cards(){
     return cards;
 }
 
-int Banker :: calculate(){
+int Banker::calculate() {
     int sum = 0;
     int count = 0;
-    for (int i = 0; i < cards.size(); i++){
-        if (cards[i][0] == 'K' || cards[i][0] =='Q' || cards[i][0] == 'J'){
+    for (int i = 0; i < cards.size(); i++) {
+        if (cards[i][1] == 'K' || cards[i][1] == 'Q' || cards[i][1] == 'J') {
             sum += 10;
         }
-        else if (cards[i][0] == 'A'){
-            count ++;
+        else if (cards[i][1] == 'A') {
+            count++;
             sum += 11;
         }
-        else{
+        else {
             int x;
-            istringstream(cards[i]) >> x;
+            istringstream(cards[i].substr(1)) >> x;
             sum += x;
         }
     }
-    while (sum > 21 && count > 0){
+    while (sum > 21 && count > 0) {
         sum -= 10;
-        count --;
+        count--;
     }
     return sum;
 }
