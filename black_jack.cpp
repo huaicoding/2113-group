e4547c93b12dfa3e1e416212d603bdb416e13267
huaicoding <131674734+huaicoding@users.noreply.github.com>
2023-05-06 11:24:05 +0800

    1

--- black_jack.cpp
+++ black_jack.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <typeinfo>
 #include <time.h> 
+#include <fstream> 
 #include "player.h"
 #include "banker.h"
 #include "AllocationSystem.h"
@@ -180,6 +181,29 @@ bool InvalidInput(string input){
         cout << " ~ Insufficient balance. Please choose your bet again." << endl;
     }
     return true;
+}
+
+void cash_out_result(int total_games, int win_count, double buy_in, double balance){
+  ofstream fout;
+  double score = buy_in - balance;
+  fout.open("cash_out_result.txt");
+  if (fout.fail()){
+    cout << "Error in opening the file !" << endl;
+    }
+  else{
+    fout << "-------------------------Game Result-----------------------------" << endl;
+    fout << "You have played " << total_games << " games in total." << endl;
+    fout << "You have won " << win_count << " games in total." << endl;
+    if (score <= 0){
+      score = -score;
+      fout << "You have won " << score << " ." << endl;
+    }
+    else if (score > 0){
+      fout << "You have lost " << score << " ." << endl;
+    }
+    fout << "-----------------------------------------------------------------" << endl;
+  }
+  fout.close();
 }
 
 int main() {
